561842c972300f4ec8ea05223ba22424d492d6a1
yamin029 <55071387+yamin029@users.noreply.github.com>
2019-11-13 07:41:57 +0600

    index and server added

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fa');
+ var server = http.createServer(function(req,res){
+       fs.readFile('index.html',function(err,data){
+            if(err){
+                  res.statusCode = 400;
+                  res.json({'err':'File not found'})
+            } 
+            res.statusCode = 200;
+            res.setHeader('Content-Type','text/html');
+            res.end(data)
+       })
+       server.listen(3000,'localhost',function(){
+             console.log('server running');
+       })
+ })
